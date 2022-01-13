9bf6dd88e01d7f02cfc8c970bade02cdc84c5abe
zfl990109 <1491657576@qq.com>
2022-01-13 18:49:36 +0800

    enter the kernel TODO:print function      set page

---
+++ kern/process/thread.h
@@ -0,0 +1,17 @@
+//
+// Created by zfl on 2022/1/13.
+//
+
+#ifndef XXOS_THREAD_H
+#define XXOS_THREAD_H
+
+#include "../../libs/defs.h"
+
+typedef void thread_func(void*);
+
+enum task_status{
+    TASK_RUNNING,
+
+};
+
+#endif //XXOS_THREAD_H
